3ff358a1aa3dc003ba2343e402292f71b299b4be
诺瓦鱼 <910709664@qq.com>
2019-09-04 18:25:16 +0800

    A Test Project

--- Role Test/Assets/Scripts/PlayerControl.cs
+++ Role Test/Assets/Scripts/PlayerControl.cs
@@ -8,7 +8,7 @@ public class PlayerControl : MonoBehaviour {
      public float moveSpeed=5f;
      public float runSpeed = 10f;
      public float gravity = 9.8f;
-     private Vector3 moveDirection;
+     private Vector3 moveDirection=Vector3.zero;
      private CharacterController playerController;
      private Animator Anim;
 
@@ -24,16 +24,15 @@ public class PlayerControl : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate ()
     {
-        
-        
+        AnimatorStateInfo animatorInfo;
+        animatorInfo = Anim.GetCurrentAnimatorStateInfo(0);
+        if (playerController.isGrounded)
+        {
             float h = Input.GetAxis("Horizontal");
             float v = Input.GetAxis("Vertical");
-
-            if (playerController.isGrounded)
-        {
             if (h != 0 || v != 0)
             {
-                Anim.SetInteger("MoveSpeed", 1);
+                Anim.SetInteger("MoveSpeed",1);
 
                 if (Input.GetKey(KeyCode.LeftShift))
                 {
@@ -52,21 +51,35 @@ public class PlayerControl : MonoBehaviour {
             }
             else
             {
-                Anim.SetInteger("MoveSpeed", -1);
+                Anim.SetInteger("MoveSpeed", 0);
             }
 
             if (Input.GetKeyDown(KeyCode.J))
             {
-                Anim.SetFloat("Atk", 1.1f);
+                Anim.SetInteger("Atk1", 1);
             }
-
-
-
-         }
-            else
+            if (animatorInfo.normalizedTime > 1f && animatorInfo.IsName("Atk1"))
             {
-                moveDirection.y = gravity * Time.deltaTime;
+                   Anim.SetInteger("Atk1",-1);
             }
+
+
+
+        }
+        else
+        {
+            
+            moveDirection.y -= gravity * Time.deltaTime;
+            playerController.Move(moveDirection * Time.deltaTime);
+        }
     }
-           
+
+    //private void Attack(AnimatorStateInfo animatorInfo)
+    //{
+    //    Anim.SetInteger("Atk1",1);
+    //    if (animatorInfo.normalizedTime > 1f && animatorInfo.IsName("Atk1"))
+    //    {
+    //        Anim.SetInteger("Atk1",-1);
+    //    }
+    //}       
 }
